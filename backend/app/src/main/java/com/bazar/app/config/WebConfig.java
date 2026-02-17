72a46ac8376cc0e98a63aa354799a4051e89f6a2
ImBenja <benjamimjuarez156@gmail.com>
2026-02-17 16:52:17 -0300

    Agregamos la Coleccion completa de POSTMAN y unos cambios en el codigo fuente, docker-compose.yml, en los .gitignore.

--- backend/app/src/main/java/com/bazar/app/config/WebConfig.java
+++ backend/app/src/main/java/com/bazar/app/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addMapping("/api/**")
         .allowedOrigins("http://localhost:5500", "http://localhost:3000", "http://127.0.0.1:5500",
             "http://127.0.0.1:3000")
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
         .allowedHeaders("*")
         .allowCredentials(true)
         .maxAge(3600);
